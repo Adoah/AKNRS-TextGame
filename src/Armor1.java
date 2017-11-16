3a9f90a67cd0c71cb7909ec81a041711884183f2
kaydenl1 <klynch@newroads.org>
2017-11-16 09:16:41 -0800

    added some armor stuff

---
+++ src/Armor1.java
@@ -0,0 +1,15 @@
+
+public class Armor1 extends Armor {
+
+	public Armor1(){
+		  super.setName("Level 1 Armor");
+		  super.setArmorClass(1);
+		  super.setSize(1);
+		  super.setWeight(1);
+		}
+		  
+		  public String toString(){
+		    return super.toString();
+		  }
+	
+}
---
+++ src/Armor1A.java
@@ -0,0 +1,15 @@
+
+public class Armor1A extends Armor {
+
+	public Armor1A(){
+		  super.setName("Level 1A Armor");
+		  super.setArmorClass(1);
+		  super.setSize(1);
+		  super.setWeight(1);
+		}
+		  
+		  public String toString(){
+		    return super.toString();
+		  }
+	
+}
