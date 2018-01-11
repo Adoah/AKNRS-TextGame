581271509adf221fdba5f737200edb8197f0d52d
kaydenl1 <klynch@newroads.org>
2018-01-11 08:11:16 -0800

    Made a Ferrari and a McClaren P1

---
+++ src/Ferrari.java
@@ -0,0 +1,13 @@
+
+public class Ferrari extends Car {
+
+	public Ferrari()
+	{
+		super("Ferrari","Ferrari 458",570);
+	}
+		  
+	 public String toString(){
+	    return super.toString();
+	  }
+	
+}
---
+++ src/P1.java
@@ -0,0 +1,13 @@
+
+public class P1 {
+
+	public P1()
+	{
+		super("P1","Mclaren p1",);
+	}
+		  
+	 public String toString(){
+	    return super.toString();
+	  }
+	
+}
