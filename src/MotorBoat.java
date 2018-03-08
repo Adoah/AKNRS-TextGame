597b10aefcd37b5d5cea3df0c38aa50c03cc4900
Kayden <klynch@newroads.org>
2018-03-08 09:18:31 -0800

    Added some boats so that we can have aquatic and marshland terrain

---
+++ src/MotorBoat.java
@@ -0,0 +1,13 @@
+
+public class MotorBoat extends Boat {
+
+	public MotorBoat()
+	{
+		super("Motor Boat","1 Dead Racoon",60);
+	}
+		  
+	 public String toString(){
+	    return super.toString();
+	  }
+	
+}
